1cf2f8a3fd7d60db970ac67e6c3161184359244b
brurs <brstupp@hotmail.com>
2017-09-18 21:32:49 -0300

    Tratamento de erros e correções de retorno

--- backend/DoacaoSangueWS/DoacaoSangueWS/Controllers/HemocentroController.cs
+++ backend/DoacaoSangueWS/DoacaoSangueWS/Controllers/HemocentroController.cs
@@ -13,72 +13,88 @@ namespace WebApplication1.Controllers
         // ((System.Data.Entity.Validation.DbEntityValidationException)$exception).EntityValidationErrors
         [HttpGet]
         [Route("hemocentro/popular")]
-        private void PopularHemocentros()
+        private HttpResponseMessage PopularHemocentros()
         {
-            var db = new DoacaoSangueEntities();
-            db.hemocentros.Add(new hemocentros() { nome = "Sangue tira sua saude Hemocentro", cidade = "Timbó", estado = "SC", complemento = "Been", cep = "89.080-260", descricao = "é nóis é do café memo" });
-            db.hemocentros.Add(new hemocentros() { nome = "IndaHemo", cidade = "Indaial", estado = "SC", complemento = "Has", cep = "87.145-987", descricao = "Complemento 2" });
-            db.hemocentros.Add(new hemocentros() { nome = "Vamos doar sangue", cidade = "Rio do Sul", estado = "SC", complemento = "Treta", cep = "87.145-987", descricao = "Complemento 3" });
-            db.hemocentros.Add(new hemocentros() { nome = "Rio do Sul Hemo Blud", cidade = "Rio do Sul", estado = "SC", complemento = "Planted", cep = "97.845-657", descricao = "Prog de Noite" });
-            db.SaveChanges();
+            try
+            {
+                var db = new DoacaoSangueEntities();
+                db.hemocentros.Add(new hemocentros() { nome = "Sangue tira sua saude Hemocentro", cidade = "Timbó", estado = "SC", complemento = "Been", cep = "89.080-260", descricao = "é nóis é do café memo" });
+                db.hemocentros.Add(new hemocentros() { nome = "IndaHemo", cidade = "Indaial", estado = "SC", complemento = "Has", cep = "87.145-987", descricao = "Complemento 2" });
+                db.hemocentros.Add(new hemocentros() { nome = "Vamos doar sangue", cidade = "Rio do Sul", estado = "SC", complemento = "Treta", cep = "87.145-987", descricao = "Complemento 3" });
+                db.hemocentros.Add(new hemocentros() { nome = "Rio do Sul Hemo Blud", cidade = "Rio do Sul", estado = "SC", complemento = "Planted", cep = "97.845-657", descricao = "Prog de Noite" });
+                db.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, $"Ocorreu um erro ao popular hemocentros: \r\n {e.Message}");
+            }
+            return Request.CreateResponse(HttpStatusCode.OK, "Base populada com sucesso!");
         }
 
         [HttpGet]
         [Route("hemocentro")]
-        public List<DoacaoSangueWS.hemocentros> RetornarHemocentros()
+        public HttpResponseMessage RetornarHemocentros()
         {
             var db = new DoacaoSangueEntities();
             var hemocentros = from b in db.hemocentros
                               orderby b.id
                               select b;
-            return hemocentros.ToList();
+
+            return Request.CreateResponse(HttpStatusCode.OK, hemocentros.ToList());
         }
 
         [HttpGet]
         [Route("hemocentro/{id:int}")]
-        public DoacaoSangueWS.hemocentros RetornarHemocentroPorId(int id)
+        public HttpResponseMessage RetornarHemocentroPorId(int id)
         {
             var db = new DoacaoSangueEntities();
             var hemocentro = (from b in db.hemocentros
                               where b.id == id
                               select b).FirstOrDefault();
-            return hemocentro;
+
+            if (hemocentro != null)
+            {
+                return Request.CreateResponse(HttpStatusCode.OK, hemocentro);
+            }
+            else
+            {
+                return Request.CreateResponse(HttpStatusCode.NotFound, "ID informado não corresponde a um Hemocentro existente");
+            }
         }
 
         [HttpGet]
         [Route("hemocentro/{nome}")]
-        public List<DoacaoSangueWS.hemocentros> RetornarHemocentrosPorNome(string nome)
+        public HttpResponseMessage RetornarHemocentrosPorNome(string nome)
         {
             var db = new DoacaoSangueEntities();
             var hemocentro = from b in db.hemocentros
-                             where b.nome == nome
+                             where b.nome.Contains(nome)
                              select b;
-            return hemocentro.ToList<DoacaoSangueWS.hemocentros>();
-        }
 
-        [HttpGet]
-        [Route("hemocentro/wildcard/{nome}")]
-        public List<DoacaoSangueWS.hemocentros> RetornarHemocentrosWildCard(string nome)
-        {
-            var db = new DoacaoSangueEntities();
-            var hemocentro = from b in db.hemocentros
-                             where b.nome == nome
-                             select b;
-            return hemocentro.ToList<DoacaoSangueWS.hemocentros>();
+            return Request.CreateResponse(HttpStatusCode.OK, hemocentro.ToList<DoacaoSangueWS.hemocentros>());
         }
 
         [HttpPost]
         [Route("hemocentro")]
-        public void InserirHemocentro([FromBody]DoacaoSangueWS.hemocentros hemocentro)
+        public HttpResponseMessage InserirHemocentro([FromBody]DoacaoSangueWS.hemocentros hemocentro)
         {
-            var db = new DoacaoSangueEntities();
-            db.hemocentros.Add(hemocentro);
-            db.SaveChanges();
+            try
+            {
+                var db = new DoacaoSangueEntities();
+                db.hemocentros.Add(hemocentro);
+                db.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, $"Ocorreu um erro ao inserir hemocentro: \r\n {e.Message}");
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, "Hemocentro incluido com sucesso");
         }
 
         [HttpPut]
         [Route("hemocentro")]
-        public void AlterarHemocentro(DoacaoSangueWS.hemocentros hemocentro)
+        public HttpResponseMessage AlterarHemocentro(DoacaoSangueWS.hemocentros hemocentro)
         {
             var db = new DoacaoSangueEntities();
             var hemocentroAux = (from b in db.hemocentros
@@ -92,13 +108,27 @@ namespace WebApplication1.Controllers
                 hemocentroAux.cidade = hemocentro.cidade != null ? hemocentro.cidade : hemocentroAux.cidade;
                 hemocentroAux.complemento = hemocentro.complemento != null ? hemocentro.complemento : hemocentroAux.complemento;
                 hemocentroAux.descricao = hemocentro.descricao != null ? hemocentro.descricao : hemocentroAux.descricao;
-                db.SaveChanges();
+
+                try
+                {
+                    db.SaveChanges();
+                }
+                catch (Exception e)
+                {
+                    return Request.CreateResponse(HttpStatusCode.BadRequest, $"Ocorreu um erro ao alterar hemocentro: \r\n {e.Message}");
+                }
             }
+            else
+            {
+                return Request.CreateResponse(HttpStatusCode.NotFound, "Hemocentro não encontrado");
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, "Hemocentro alterado com sucesso");
         }
 
         [HttpDelete]
         [Route("hemocentro/{id:int}")]
-        public void ExcluirHemocentro(int id)
+        public HttpResponseMessage ExcluirHemocentro(int id)
         {
             var db = new DoacaoSangueEntities();
             var hemocentro = db.hemocentros.Where(x => x.id == id).FirstOrDefault();
@@ -107,6 +137,12 @@ namespace WebApplication1.Controllers
                 db.hemocentros.Remove(hemocentro);
                 db.SaveChanges();
             }
+            else
+            {
+                return Request.CreateResponse(HttpStatusCode.NotFound, "Hemocentro não encontrado");
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, "Hemocentro alterado com sucesso");
         }
 
     }
